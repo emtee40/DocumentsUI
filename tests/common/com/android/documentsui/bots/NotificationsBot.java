140ac00d89e83b4047af56d29d7b29c7f145f54e
Jun Ono <jun.x.ono@sony.com>
2017-12-08 19:07:55 +0900

    Add test for file copy operation between Internal and External storage
    Added test that checks if a large number of file copy between the
    different storage media (Internal Storage and SD Card) works as
    expected.
    
    It uses a physical SD Card media if it's inserted, or uses virtual SD
    Card if the test device doesn't have SD Card slot or not inserted.
    
    Bug: 72360396
    Test: Run instrumentation tests from com.android.documentsui.tests
    Change-Id: I1f859ceb68b89746fefaec27e11e530f10c84ab5

---
+++ tests/common/com/android/documentsui/bots/NotificationsBot.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.documentsui.bots;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+
+/**
+ * A test helper class for controlling notification items.
+ */
+public class NotificationsBot extends Bots.BaseBot {
+
+    public NotificationsBot(UiDevice device, Context context, int timeout) {
+        super(device, context, timeout);
+    }
+
+    public void setNotificationAccess(
+            Activity activity, String appName, String label) throws UiObjectNotFoundException {
+        Intent intent = new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS);
+        activity.startActivity(intent);
+        mDevice.waitForIdle();
+
+        clickLabel(appName);
+
+        clickLabel(label);
+        mDevice.pressKeyCode(KeyEvent.KEYCODE_BACK);
+        mDevice.waitForIdle();
+    }
+
+    public boolean isNotificationAccessEnabled(ContentResolver resolver, String pkgName) {
+        String listeners = Settings.Secure.getString(resolver, "enabled_notification_listeners");
+        if (!TextUtils.isEmpty(listeners)) {
+            String[] list = listeners.split(":");
+            for(String item : list) {
+                if(item.startsWith(pkgName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void clickLabel(String label) throws UiObjectNotFoundException {
+        UiSelector selector = new UiSelector().textMatches("(?i)" + label);
+        mDevice.findObject(selector).click();
+        mDevice.waitForIdle();
+    }
+}
+
