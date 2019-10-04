e5933237a0296b3710b142feafbbe07c55dec3ad
Kun Niu <kunniu@google.com>
2019-10-04 17:06:00 +0000

    Revert "Track MimeMap moving to libcore.content.type."
    This reverts commit 65fc42449bfa8a0fcd857852c0ed362dff6b41d4.
    
    Reason for revert: break git_qt-qpr1-dev-plus-aosp build
    
    Change-Id: Ia4e3a23c9b495d1cd9e97c5279baa0b812414102

--- tests/common/com/android/documentsui/testing/TestModel.java
+++ tests/common/com/android/documentsui/testing/TestModel.java
@@ -27,7 +27,7 @@ import com.android.documentsui.base.DocumentInfo;
 import com.android.documentsui.base.Features;
 import com.android.documentsui.roots.RootCursorWrapper;
 
-import libcore.content.type.MimeMap;
+import libcore.net.MimeMap;
 
 import java.util.Random;
 
